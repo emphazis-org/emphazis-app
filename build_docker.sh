b39bfcd6490940ca3a502c3312a31c34b84ba9a4
luciorq <luciorqueiroz@gmail.com>
2021-03-23 20:50:48 -0300

    feat: use docker buildx

--- build_docker.sh
+++ build_docker.sh
@@ -5,7 +5,7 @@ docker rm emphazis-app
 docker rmi luciorq/emphazis-app:latest
 
 # to build
-docker build . -f dockerfile/Dockerfile -t luciorq/emphazis-app:latest
+DOCKER_BUILDKIT=1 docker build . -f dockerfile/Dockerfile -t luciorq/emphazis-app:latest
 
 # to run
 docker run -d \
